6868077a443adbf5e40ae797a0875985415eb8d4
Zachary Baird <zachary.baird@alpinetesting.com>
2022-07-08 11:27:34 -0400

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use clipboard::ClipboardContext;
+use clipboard::ClipboardProvider;
+use colored::Colorize;
+use guid_create::GUID;
+
+fn main() {
+    // Assign clipboard ctx and init result
+    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+    let mut result: String = String::new();
+
+    let guid = GUID::rand().to_string();
+    let guid = guid.split("-"); // shadow variable
+
+    for slice in guid {
+        result += slice;
+    }
+
+    ctx.set_contents(result.to_owned()).unwrap();
+    println!("Guid: {result}");
+    println!("{}", "Copied to clipboard.".green().bold());
+}
